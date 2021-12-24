bd0d2019d6897ee98a1d341187a1cca1698cac49
Rajan Khade <rajankhade31@gmail.com>
2021-12-25 00:19:23 +0530

    used summernote

--- main.js
+++ main.js
@@ -1,22 +1,3 @@
-function handleRef(t) {
-    let e = document.getElementById('ref'); 
-    if (e.disabled == false) { 
-        e.disabled = true
-        t.innerHTML = 'enable' 
-    } else { 
-        e.disabled = false
-        t.innerHTML = 'disable'
-    }
-}
-
-let m = document.getElementById("main");
-m.value = localStorage.main
-m.addEventListener("change", function() {
-    localStorage.main = m.value
-})
-
-let r = document.getElementById("ref");
-r.value = localStorage.ref
-r.addEventListener("change", function() {
-    localStorage.ref = r.value
-})
+$(document).ready(function () {
+    
+});
